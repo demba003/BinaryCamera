8d0ba957eea3c5e692188a4a93e84d4852c1b3fc
kamil <kd.debinski@gmail.com>
2019-04-16 22:27:06 +0200

    ui updates

--- app/src/main/java/pl/padm/binarycamera/CameraActivity.kt
+++ app/src/main/java/pl/padm/binarycamera/CameraActivity.kt
@@ -6,9 +6,14 @@ import android.graphics.SurfaceTexture
 import android.hardware.Camera
 import android.hardware.Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_camera.*
 import pl.padm.binarycamera.camera.PreviewCallback
 import pl.padm.binarycamera.camera.SurfaceListener
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
+
 
 class CameraActivity : Activity() {
     private lateinit var camera: Camera
@@ -17,6 +22,7 @@ class CameraActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         textureView.surfaceTextureListener = SurfaceListener(this::updateFps, this::initCamera)
+        initSeekBar()
     }
 
     override fun onDestroy() {
@@ -36,13 +42,35 @@ class CameraActivity : Activity() {
 
     private fun updateFps(fps: Double) {
         this@CameraActivity.runOnUiThread {
-            fpsCounter.text = with("%2.2f") { format(fps) }
+            fpsCounterOriginal.text = with("%2.2f") { format(fps) }
         }
     }
 
     private fun updateProcessedFps(fps: Double) {
         this@CameraActivity.runOnUiThread {
-            fpsCounter2.text = with("%2.2f") { format(fps) }
+            fpsCounterProcessed.text = with("%2.2f") { format(fps) }
+        }
+    }
+
+    private fun initSeekBar() {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                factor.text = with("%1.2f") { format(progress / 100.0) }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    private fun initResolutionPicker(sizes: List<Camera.Size>) {
+        val list = sizes.map { "${it.width}x${it.height}" }.toList()
+        resolutions.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list)
+
+        resolutions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
         }
     }
 
@@ -51,10 +79,9 @@ class CameraActivity : Activity() {
 
         val parameters = camera.parameters
         parameters.supportedPreviewSizes.forEach { println("${it.width}x${it.height}") }
-
         parameters.setRecordingHint(true)
         parameters.colorEffect = Camera.Parameters.EFFECT_MONO
-        parameters.setPreviewSize(1920, 1080)
+        parameters.setPreviewSize(1280, 720)
         parameters.focusMode = FOCUS_MODE_CONTINUOUS_PICTURE
         camera.parameters = parameters
 
@@ -68,6 +95,7 @@ class CameraActivity : Activity() {
             )
         )
 
+        initResolutionPicker(parameters.supportedPreviewSizes)
         camera.startPreview()
     }
 }
